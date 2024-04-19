5517e07edd413c2f0e05470281076844fca8a5a5
selfcan <99958756+selfcan@users.noreply.github.com>
2024-04-19 19:44:38 +0800

    Update diy-part2.sh

--- scripts/common/diy-part2.sh
+++ scripts/common/diy-part2.sh
@@ -18,3 +18,11 @@ echo -e 'CONFIG_DEVEL=y\nCONFIG_CCACHE=y' >> .config
 
 # design修改proxy链接
 sed -i -r "s#navbar_proxy = 'openclash'#navbar_proxy = 'passwall'#g" feeds/luci/themes/luci-theme-design/luasrc/view/themes/design/header.htm
+add luci-app-autotimeset
+git clone https://github.com/sirpdboy/luci-app-autotimeset.git package/luci-app-autotimeset
+git clone https://github.com/selfcan/luci-app-fileassistant.git package/luci-app-fileassistant
+git clone https://github.com/selfcan/luci-app-parentcontrol.git package/luci-app-parentcontrol
+echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> "feeds.conf.default"
+echo "src-git small https://github.com/kenzok8/small" >> "feeds.conf.default"
+./scripts/feeds update -a
+./scripts/feeds install -a
